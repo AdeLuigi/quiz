b4666ec6ce740ae9d71ef9d85c6a8a876f5751d6
Ademario Santana <ademario63@gmail.com>
2022-06-26 23:31:50 -0300

    trab do felipe

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import React, { useState } from 'react';
 
+import IMG5 from "./pergunta5.png"
+
 export default function App() {
 	const questions = [
 		{
-			questionText: 'Alan Turing é conhecido como ____',
+			questionText: 'Tales calculou a altura de uma pirâmide, utilizando uma relação de proporcionalidade entre:',
 			answerOptions: [
-				{ answerText: 'Pai da biologia', isCorrect: false },
-				{ answerText: 'Pai da física', isCorrect: false },
-				{ answerText: 'Pai da computação', isCorrect: true },
-				{ answerText: 'Pai da matemática', isCorrect: false },
+				{ answerText: 'Feixe de retas paralelas.', isCorrect: false },
+				{ answerText: 'Três triângulos.', isCorrect: false },
+				{ answerText: 'Dois quadriláteros.', isCorrect: false },
+				{ answerText: 'Dois triângulos.', isCorrect: true },
 			],
 		},
 		{
-			questionText: 'A máquina de Turing ajudou a _____?',
+			questionText: 'Outra propriedade que foi importante para que Tales pudesse calcular a altura da pirâmide foi a semelhança de triângulos. Essa propriedade determina que dois triângulos são semelhantes quando: ',
 			answerOptions: [
-				{ answerText: 'Acessar a internet', isCorrect: false },
-				{ answerText: 'Decifrar os códigos criptografados', isCorrect: true },
-				{ answerText: 'Jogar jogos', isCorrect: false },
-				{ answerText: 'Fritar batata', isCorrect: false },
+				{ answerText: 'Seus ângulos são diferentes mas seus lados têm o mesmo tamanho.', isCorrect: false },
+				{ answerText: 'Seus ângulos são iguais e seus lados são ordenadamente proporcionais.', isCorrect: true },
+				{ answerText: 'Seus ângulos são iguais e seus lados têm o mesmo tamanho.', isCorrect: false },
+				{ answerText: 'Seus ângulos são diferentes e seus lados são de tamanhos diferentes.', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'A máquina que Turing criou é considerada o ______',
+			questionText: 'Além do teorema que leva seu nome, Proclo também atribui a Tales de Mileto outras proposições matemáticas como:',
 			answerOptions: [
-				{ answerText: 'Primeiro computador', isCorrect: true },
-				{ answerText: 'Melhor video game', isCorrect: false },
-				{ answerText: 'Primeiro robô', isCorrect: false },
-				{ answerText: 'Smartphone', isCorrect: false },
+				{ answerText: 'O Teorema de Pitágoras', isCorrect: false },
+				{ answerText: 'A lei dos senos', isCorrect: false },
+				{ answerText: 'A congruência entre ângulos opostos por um vértice', isCorrect: true },
+				{ answerText: 'O teorema de sanduíche (Teorema do confronto)', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Alan Turing nasceu em _____',
+			questionText: 'Agora que já sabemos quem foi Tales de Mileto, e sua importância para a matemática, só nos resta aprender a aplicar seu teorema. Mas que graça tem medir uma piramide que nunca vimos na vida não é mesmo? Então vamos medir o Cristo Redentor, sabendo que em determinado momento do dia, uma pessoa de 1,75m forma no solo uma sombra de 1,20m, e que neste mesmo momento, o Cristo forma no solo uma sombra de aproximadamente 26m, qual será então a altura do Cristo.',
 			answerOptions: [
-				{ answerText: '1911', isCorrect: false },
-				{ answerText: '1900', isCorrect: false },
-				{ answerText: '1840', isCorrect: false },
-				{ answerText: '1912', isCorrect: true },
+				{ answerText: '38m', isCorrect: true },
+				{ answerText: '53m', isCorrect: false },
+				{ answerText: '29m', isCorrect: false },
+				{ answerText: '42m', isCorrect: false },
 			],
 		},
 		{
-			questionText: 'Alan Turing era noivo de _____',
+			questionText: 'Dado o triângulo a seguir e sabendo que o segmento DE é paralelo à base CB e que AC mede 10 cm, AD mede 4 cm e AE é igual a 5 cm, então podemos afirmar que o segmento BE mede:',
 			answerOptions: [
 				{ answerText: 'Beyoncé', isCorrect: false },
 				{ answerText: 'Anitta', isCorrect: false },
@@ -71,6 +73,12 @@ export default function App() {
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
 
+	function chamaImg(index){
+		if (index == 4) {
+			return <img src={IMG5} width={400}  />
+		}
+	}
+
 	const handleAnswerOptionClick = (isCorrect) => {
 		if (isCorrect) {
 			setScore(score + 1);
@@ -83,6 +91,7 @@ export default function App() {
 			setShowScore(true);
 		}
 	};
+
 	return (
 		<div className='app'>
 			{showScore ? (
@@ -92,20 +101,23 @@ export default function App() {
 						<h3>Respostas</h3>
 					</div>
 					<div style={{display:'flex', flexDirection:'column'}}> 
-						<h5>1 - Alan Turing é conhecido como pai da ciencia da computação</h5>
-						<p>Matemático e cientista, Alan Turing nasceu em Londres em 23 de junho de 1912 e é conhecido como o pai da computação e da inteligência artificial.</p>
+						<h5>1 - Tales calculou a altura de uma pirâmide, utilizando uma relação de proporcionalidade entre</h5>
+						<p>Em sua Vida dos Filósofos Ilustres, Diógenes Laércio (que viveu por volta do século III) é mais explícito quanto ao que Tales teria feito; segundo ele ninguém o instruiu mas, tendo ido ao Egito, passou um tempo próximo aos sacerdotes de lá. Hieronimus nos diz que mediu as pirâmides a partir de suas sombras, observando quando nossas sombras são iguais a nós mesmos.
+Do nosso ponto de vista, o que Tales fez foi usar a semelhança entre os triângulos retângulos. Entretanto, àquela altura, é quase certo que esta propriedade não fosse conhecida de ninguém. Infelizmente, nada no registro histórico nos permite determinar como Tales teria chegado à conclusão sobre a igualdade das sombras. O melhor que podemos fazer é tentar criar uma hipótese plausível que, ainda que não seja historicamente justificável, nos ajuda a entender como ele pode ter tido a ideia certa. 
+	Uma possibilidade é que Tales tenha usado uma argumentação baseada nas áreas dos dois triângulos pois os egípcios já sabiam como medir estas áreas. Segundo esta interpretação, Tales teria preenchido o triângulo cuja altura é igual à da pirâmide com várias cópias do triângulo cuja altura coincide com a do próprio Tales. Tendo feito isto, teria observado que a altura da pirâmide, medida em termos da altura de Tales, é igual ao comprimento da sombra da pirâmide, medido em termos da sombra de Tales. Escolhendo o momento em que sua sombra tem o mesmo comprimento de sua altura, este argumento permitiria a Tales concluir que o mesmo tem que valer para a sombra e a altura da pirâmide. 
+	Embora esta ideia de como Tales teria procedido seja bastante atraente, é importante insistir que não há nenhum documento histórico que nos permita determinar se foi mesmo isto que aconteceu.</p>
 					</div>
 					<div style={{display:'flex', flexDirection:'column'}}> 
-						<h5>2 - A máquina de Turing ajudou a decifrar os códigos criptografados</h5>
-						<p>A máquina eletromecânica que ele inventou durante a Segunda Guerra, a "máquina de Turing", ajudou a decifrar os códigos criptografados pela máquina Enigma, usada pela marinha da Alemanha nazista. A invenção de Turing é apontada como um dos motivos da vitória dos Aliados e é considerada um fator para guerra não ter se estendido ainda por mais anos — o que causaria ainda mais mortes.</p>
+						<h5>2 - Outra propriedade que foi importante para que Tales pudesse calcular a altura da pirâmide foi a semelhança de triângulos. Essa propriedade determina que dois triângulos são semelhantes quando seus ângulos são iguais e seus lados são ordenadamente proporcionais. </h5>
+						<p>Dados dois triângulos ABC e A’B’C’, vamos dizer que eles são semelhantes se, e somente se, os ângulos correspondentes são congruentes na mesma ordem, ou seja, se os ângulos são iguais e se os lados correspondentes são ordenadamente proporcionais.</p>
 					</div>
 					<div style={{display:'flex', flexDirection:'column'}}> 
-						<h5>3 - A máquina que Turing criou é considerada o primeiro computador</h5>
-						<p>Turing refinou os conceitos de Babbage e Von Neumann ao teorizar unidades de armazenamento, desenvolveu o Colossus, o primeiro computador digital programável do mundo, e publicou trabalhos sobre Inteligência Artificial, tendo proposto o teste para definir se uma IA é capaz de se passar por um humano.</p>
+						<h5>3 - Além do teorema que leva seu nome, Proclo também atribui a Tales de Mileto outras proposições matemáticas como a congruência entre ângulos opostos por um vértice </h5>
+						<p>Proclus atribui a Tales haver afirmado ou demonstrado pela primeira vez que um ângulo inscrito numa semicircunferência é reto; que os ângulos opostos pelo vértice são iguais; que os ângulos da base de um triângulo isósceles são iguais; que um círculo é dividido igualmente pelo seu diâmetro; que se dois triângulos são tais que dois ângulos e um lado de um são iguais respectivamente a dois ângulos e um lado do outro, então os triângulos são congruentes.</p>
 					</div>
 					<div style={{display:'flex', flexDirection:'column'}}> 
-						<h5>4 - Alan Turing nasceu em 1912</h5>
-						<p>23 de junho de 1912, Maida Vale, Londres, Reino Unido</p>
+						<h5>4 - Agora que já sabemos quem foi Tales de Mileto, e sua importância para a matemática, só nos resta aprender a aplicar seu teorema. Mas que graça tem medir uma piramide que nunca vimos na vida não é mesmo? Então vamos medir o Cristo Redentor, sabendo que em determinado momento do dia, uma pessoa de 1,75m forma no solo uma sombra de 1,20m, e que neste mesmo momento, o Cristo forma no solo uma sombra de aproximadamente 26m, qual será então a altura do Cristo.</h5>
+						<p>42m</p>
 					</div>
 					<div style={{display:'flex', flexDirection:'column'}}> 
 						<h5>5 - Alan Turing era noivo de Joan Clarke</h5>
@@ -126,6 +138,11 @@ export default function App() {
 						<div className='question-count'>
 							<span>Pergunta {currentQuestion + 1}</span>/{questions.length}
 						</div>
+
+						{chamaImg(currentQuestion+1)}
+
+						{console.log(questions[currentQuestion])}
+						
 						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
